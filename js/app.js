71d221f1198bf311ad660c8a89fed2ac4632e40d
Dilshod <dnumonov49@gmail.com>
2025-12-25 16:14:53 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,3 +6,20 @@
         clickable: true,
       },
     });
+let box = document.querySelector('.box');
+
+axios.get('https://test-12b9d-default-rtdb.europe-west1.firebasedatabase.app/.json')
+.then(res => {
+  console.log(res.data);
+
+  let cound = {
+    img: '../img/comir.png',
+    id: '0',
+    name: 'L-набор Classic'
+  };
+
+  return axios.post(
+    'https://test-12b9d-default-rtdb.europe-west1.firebasedatabase.app/.json',
+    cound
+  );
+});
